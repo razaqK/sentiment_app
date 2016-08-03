3b7fdcb9b272bdda667077bfa5de2a0ce5ffd7ef
razaqk <abdrkasali@gmail.com>
2016-08-03 05:37:13 +0100

    featured mthd added

--- src/utils/classifier.py
+++ src/utils/classifier.py
@@ -4,9 +4,17 @@ from textblob.sentiments import *
 
 class TextClassifier:
 
-    TRAIN_DATA = []
+    _TRAIN_DATA = []
 
     TEST_DATA = []
 
-    cl = NaiveBayesClassifier(TRAIN_DATA)
+    def feature_words(self):
+        pass
+
+    def extract_feature_words(self):
+        pass
+
+    def train_feature_words(self):
+        cl = NaiveBayesClassifier(self._TRAIN_DATA)
+        pass
     pass
