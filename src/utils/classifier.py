c9033f13345440220cc12c1e9c9dafbf679f7656
razaqk <abdrkasali@gmail.com>
2016-08-04 06:47:16 +0100

    classifier improved

--- src/utils/classifier.py
+++ src/utils/classifier.py
@@ -4,9 +4,24 @@ from textblob.sentiments import *
 
 class TextClassifier:
 
-    _TRAIN_DATA = []
+    _TRAIN_DATA = [('I love this sandwich.', 'pos'),
+         ('this is an amazing place!', 'pos'),
+         ('I feel very good about these beers.', 'pos'),
+         ('this is my best work.', 'pos'),
+         ("what an awesome view", 'pos'),
+         ('I do not like this restaurant', 'neg'),
+         ('I am tired of this stuff.', 'neg'),
+         ("I can't deal with this", 'neg'),
+         ('he is my sworn enemy!', 'neg'),
+         ('my boss is horrible.', 'neg'),
+         ("He is a bad.", 'neg')]
 
-    TEST_DATA = []
+    TEST_DATA = [('the beer was good.', 'pos'),
+        ('I do not enjoy my job', 'neg'),
+        ("I ain't feeling dandy today.", 'neg'),
+        ("I feel amazing!", 'pos'),
+        ('Gary is a friend of mine.', 'pos'),
+        ("I can't believe I'm doing this.", 'neg')]
 
     def feature_words(self):
         pass
@@ -16,5 +31,5 @@ class TextClassifier:
 
     def train_feature_words(self):
         cl = NaiveBayesClassifier(self._TRAIN_DATA)
+        blob = TextBlob("The beer is good. But the bad hangover.", classifier=cl)
         pass
-    pass
