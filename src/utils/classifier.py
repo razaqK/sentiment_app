6586dddf35a4d30d4c98409a825f9b6ee87f9f20
razaqk <abdrkasali@gmail.com>
2016-08-19 05:33:11 +0100

    update

--- src/utils/classifier.py
+++ src/utils/classifier.py
@@ -3,31 +3,31 @@ from textblob import TextBlob
 import json
 from textblob.sentiments import *
 
+
 class TextClassifier:
 
     _TRAIN_DATA = [('I love this sandwich.', 'pos'),
-         ('this is an amazing place!', 'pos'),
-         ('I feel very good about these beers.', 'pos'),
-         ('this is my best work.', 'pos'),
-         ("what an awesome view", 'pos'),
-         ('I do not like this restaurant', 'neg'),
-         ('I am tired of this stuff.', 'neg'),
-         ("I can't deal with this", 'neg'),
-         ('he is my sworn enemy!', 'neg'),
-         ('my boss is horrible.', 'neg'),
-         ("He is a bad.", 'neg')]
+                   ('this is an amazing place!', 'pos'),
+                   ('I feel very good about these beers.', 'pos'),
+                   ('this is my best work.', 'pos'),
+                   ("what an awesome view", 'pos'),
+                   ('I do not like this restaurant', 'neg'),
+                   ('I am tired of this stuff.', 'neg'),
+                   ("I can't deal with this", 'neg'),
+                   ('he is my sworn enemy!', 'neg'),
+                   ('my boss is horrible.', 'neg'),
+                   ("He is a bad.", 'neg')]
 
     TEST_DATA = [('the beer was good.', 'pos'),
-        ('I do not enjoy my job', 'neg'),
-        ("I ain't feeling dandy today.", 'neg'),
-        ("I feel amazing!", 'pos'),
-        ('Gary is a friend of mine.', 'pos'),
-        ("I can't believe I'm doing this.", 'neg')]
+                 ('I do not enjoy my job', 'neg'),
+                 ("I ain't feeling dandy today.", 'neg'),
+                 ("I feel amazing!", 'pos'),
+                 ('Gary is a friend of mine.', 'pos'),
+                 ("I can't believe I'm doing this.", 'neg')]
 
     cl = NaiveBayesClassifier(_TRAIN_DATA)
 
-
-    def load_words_in_list(self, filename):
+    def load_words_list(self, filename):
         pass
 
     def feature_words(self):
@@ -48,18 +48,17 @@ class TextClassifier:
     def check_sent_accuracy(self):
         self.cl.accuracy(self.TEST_DATA)
 
-    def clasiffy_sent(self, sentence):
+    def clasify_sent(self, sentence):
         self.cl.classify(sentence)
 
     def check_sent(self):
         print(self.cl.classify("He ain't from around here.!"))
-
         prob_dist = self.cl.prob_classify("This one's a doozy.")
         prob_dist.max()
         round(prob_dist.prob("pos"), 2)
         round(prob_dist.prob("neg"), 2)
-        
-    def sentiment(self, message):
+
+    def get_sentiment(self, message):
         text = TextBlob(message)
         response = {'polarity': text.polarity, 'subjectivity': text.subjectivity}
         return json(response)
